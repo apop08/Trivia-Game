186eaf50bcc28dcd752346eae16c706f3365bfb5
Alex Poplawski <alex0poplawski@gmail.com>
2019-03-28 18:35:01 -0700

    finalized the game and commented code

--- Assets/Javascript/TriviaGame.js
+++ Assets/Javascript/TriviaGame.js
@@ -4,20 +4,21 @@
 * date	3/21/2019
 * authors: Alex Poplawski
 * brief
-The javascript file for the trivia handles all the game logic and drawing
+The javascript file for the trivia handles all the game logic and drawing for trivia game
 */
 /**************************************************************************************************/
 
 // JavaScript function that wraps everything
 $(document).ready(function() 
 {
+    //time per question constant
+    const timePerQ = 30;
     /*
         the game object singleton
     */
-    const timePerQ = 30;
     var gameInfo = {
-        timer: 0,
-        gameState: "",
+        timer: 0, 
+        gameState: "", 
         curQuestion: null,
         questions: 0,
         right: 0,
@@ -26,6 +27,10 @@ $(document).ready(function()
         intervalId: null,
         clockRunning: false,
         questionArray: [],
+
+        /*
+        * Starts the timer and sets that it is running sets the interval as well
+        */
         startTimer: function()
         {
             if (!this.clockRunning) 
@@ -38,31 +43,40 @@ $(document).ready(function()
             }          
         },
 
+        /*
+        * Stops the timer and clears the interval/switch
+        */
         stopTimer: function() 
         {
             clearInterval(this.intervalId);
             this.clockRunning = false;
         },
 
+        /*
+        * decreases the timer and runs the logic if timer is 0
+        */
         decTimer: function() 
         {
             --this.timer;
             $("#Timer").html("Time: " + this.timer);
-            $('#hands').css( {"transform": "rotate(" + gameInfo.timer * 12 + "deg)"});
             if(this.timer <= 0)
             {
                 this.submitAns();
             }
         },
 
+        /*
+        * handles the logic for checking answer
+        */
         submitAns: function()
         {
-            let selected = $("input[type='radio']:checked");
-            let ans = selected.val();
-            if(ans || this.timer == 0)
+            let selected = $("input[type='radio']:checked"); //button pressed
+            let ans = selected.val(); //value of button pressed
+            if(ans || this.timer == 0) // if an answer was pressed or timer is 0
             {
+                //check the answer
                 let pointsEarned = this.curQuestion.submit(ans);
-                if(pointsEarned > 0)
+                if(pointsEarned > 0) //increment number of questions right or wrong
                 {
                     ++this.right;
                 }
@@ -71,7 +85,7 @@ $(document).ready(function()
                     ++this.wrong;
                 }
                 this.score += pointsEarned;
-
+                //if all the questions are used
                 if(this.questions == this.right + this.wrong && this.questions != 0)
                 {
                     this.endQuiz();
@@ -82,6 +96,11 @@ $(document).ready(function()
             }
         },
 
+        /*
+        * creates an api string based on the inputs given when the button is pressed
+
+        * return: a string of the given parameters set for the api call
+        */
         createApiCallString: function()
         {
             var amount = $("#trivia_amount").val();
@@ -105,10 +124,13 @@ $(document).ready(function()
             return apiString;
         },
 
+        /*
+        * populates the question array based on the response from the api call
+        */
         populateQuestionArray: function()
         {            
             let request = new XMLHttpRequest();
-            var apiString = "https://opentdb.com/api.php?" + this.createApiCallString();
+            let apiString = "https://opentdb.com/api.php?" + this.createApiCallString();
             
             request.open("GET", apiString, true);
             
@@ -117,10 +139,11 @@ $(document).ready(function()
                 var data = JSON.parse(this.response)
                 data["results"].forEach(ques => 
                 {
-                    let arr = ques["incorrect_answers"];
+                    let arr = ques["incorrect_answers"]; 
                     let cat = ques["category"]
                     let value = 0;
-                    arr.push(ques["correct_answer"]);
+                    arr.push(ques["correct_answer"]); // add the correct answer to the incorrect answers so we have all possible selections
+                    //sets point value of question based on how difficult the api considers it
                     if(ques["difficulty"] == "easy")
                     {
                         value = 1;
@@ -133,7 +156,7 @@ $(document).ready(function()
                     {
                         value = 3;
                     }
-                    
+                    // use the truefalse class instead of question if its a boolean
                     if(ques["type" == "boolean"])
                     {
                         var q = new trueFalse(ques["question"], arr, ques["correct_answer"], timePerQ, value, cat);
@@ -142,9 +165,8 @@ $(document).ready(function()
                     {
                         var q = new question(ques["question"], arr, ques["correct_answer"], timePerQ, value, cat);
                     }
-                    gameInfo.questionArray.push(q);
+                    gameInfo.questionArray.push(q); //add question to the array
                 })
-
                 gameInfo.getNewQuestion()
                 gameInfo.draw();
                
@@ -153,7 +175,9 @@ $(document).ready(function()
             request.send();
             
         },
-
+        /*
+        * ends of the quiz screen draw
+        */
         endQuiz: function()
         {
             this.stopTimer();
@@ -168,6 +192,9 @@ $(document).ready(function()
             })
         },
 
+        /*
+        * restarts the game
+        */
         reset: function()
         {
             this.timer = 0;
@@ -181,6 +208,11 @@ $(document).ready(function()
             this.questionArray = [];
         },
 
+        /*
+        * gets a next question from the array
+
+        * return: if there is a next question or not
+        */
         getNewQuestion: function()
         {
             this.curQuestion = this.questionArray.pop();
@@ -193,11 +225,17 @@ $(document).ready(function()
             
         },
 
+        /*
+        * updates the stats table
+        */
         updateStats: function()
         {
             $("#Stats").html("Right: " + this.right + " Wrong: " + this.wrong + " Score: " + this.score);
         },
 
+        /*
+        * draws the main game state
+        */
         draw: function()
         {
             $("#Intro").hide();
@@ -214,7 +252,14 @@ $(document).ready(function()
             })
         }
     };
+
+    /*
+        * object containing the logic for the default screen
+    */
     var defaultScreen = {
+        /*
+        * draws the initial screen gets the possible categories and inserts into the list
+        */
         draw: function()
         {
             let categories = new XMLHttpRequest();
@@ -244,11 +289,21 @@ $(document).ready(function()
 
     }
     /*
-
+    * The question class handles all logic for the question
     */
     class question 
     {
+        /*
+        * question object contructor
 
+        * params:
+        * qText - The question string
+        * responses - array of strings containing possible responses
+        * correctAns - the string containing the right answer
+        * time - time for question
+        * score - how much question is worth based on difficulty
+        * cat - the category string
+        */
         constructor(qText, responses, correctAns, time, score, cat) 
         {
             this.questionText = qText;
@@ -259,6 +314,15 @@ $(document).ready(function()
             this.category = cat;
         }
 
+        /*
+        * shuffle a given array
+
+        * params:
+        * arr - array that needs to be shuffles
+
+        * return:
+        * return the array
+        */
         shuffle(arr)
         {
             for (let i = arr.length - 1; i > 0; i--) 
@@ -269,6 +333,15 @@ $(document).ready(function()
             return arr;
         }
 
+        /*
+        * submits a given question
+
+        * params:
+        * ans - the answer to check
+
+        * return:
+        * the points earned or 0 if its wrong
+        */
         submit(ans)
         {
             if(ans)
@@ -279,6 +352,16 @@ $(document).ready(function()
                         
         }
 
+
+        /*
+        * checks a given answer if its correct
+
+        * params:
+        * ans - the answer to check
+
+        * return:
+        * the points earned or 0 if its wrong
+        */
         checkAns(ans)
         {
             // why cant i use == here? this works but could potentially cause false positives
@@ -289,20 +372,33 @@ $(document).ready(function()
             return 0;
         }
 
+        /*
+        * getter for time
+
+        * return: time of question
+        */
         getTime()
         {
             return this.qTime;
         }
 
+        /*
+        * getter for value of question
+
+        * return: worth of the question
+        */
         getWorth()
         {
             return this.worth;
         }
 
+        /*
+        * draws the question part of the screen
+        */
         draw()
         {
             
-            $("#qText").html("Category: " + this.category +  "<br>" + this.questionText);
+            $("#qText").html("Category: " + this.category +  "<br>" + this.questionText );
             var res = $("#responses")
             res.empty();
             
@@ -311,11 +407,14 @@ $(document).ready(function()
             {
                 res.append("<input type=\"radio\" name=\"choice\" value=\"" + this.possibleResponses[i] + "\" required>" + this.possibleResponses[i] + "<br>");
             }
-            res.append("<input class=\"btn btn-primary\" type=\"submit\" value=\"submit\" id=\"submit\">");
+            res.append("<input class=\"btn btn-primary\" type=\"submit\" value=\"Submit\" id=\"submit\">");
             res.append("</form>");
         }
     }
 
+    /*
+    * true or false question that inherits from question not really necessary anymore since the api doesnt do a good job of sorting the questions
+    */
     class trueFalse extends question
     {
         constructor(qText, responses, correctAns, time, score, cat)
